c74480b7b0c17342ea6d2f28f755c6fed2072518
s3cana <18374819636@163.com>
2022-03-18 17:07:53 +0800

    354/353

--- recur/10/main.c
+++ recur/10/main.c
@@ -6,47 +6,42 @@ bool istrue(char *p,char *s){
     return  *p== *s || *p == '.'; 
 
 }
-
-bool isMatch(char * s, char * p){
-    if(*(p+1)=='\0' && *(s+1) == '\0' && (istrue(p,s) ||(istrue(p-1,s) && *p=='*'))){
-        return true;
-    }else{
-        if( *p == *s || *p == '.'){
-            if(*(s+1)!='\0' && *(p+1)!='\0')
-                return isMatch(s+1,p+1);
-        }else if(*p != *s && *p == '*'){
-            if( istrue(p-1,s) && istrue(p+1,s+1)){//后面一个也匹配到了
-                if(*(s+1)!='\0' && *(p+1) !='\0')
-                return isMatch(s+1,p+1);
-            }else if(istrue(p-1,s) && !istrue(p+1,s+1)){
-                /*前一个匹配到了，后一个没匹配到 */
-                bool a=0,b=0;
-                if(*(p+1)!='\0'){
-                    a = isMatch(s,p+1);
-                }
-                if(*(s+1)!='\0'){
-                    b = isMatch(s+1,p);// '\0' *
-                }
-                return a || b;
-            }else if(!istrue(p-1,s)){
-                if(*(p+1)!='\0')
-                    return isMatch(s,p+1);
-            }
-        }else if(*p!=*s&&*(p+1)!='\0'&&*(p+1)=='*'){
-            p++;
-            printf("eee\n");
-            if(*(p+1)!='\0')
-            return isMatch(s,++p);
-        }else{
-            return false;
-        }
-
-    }
-    return false;
+bool isNULL(char *p){
+    return *p == '\0';
 }
+bool isMatch(char *s ,char* p){
+    if(*s == *p || *p== '.'){
+        if(*p=='\0' && *s== '\0'){
+            return true;
+        }else if (isNULL(s)){
+            return isMatch(s,p+1);
+        }else if(isNULL(p)){
+            return isMatch(s+1,p);
+        }else return isMatch(s+1,p+1);
+    }else{
+        if(isNULL(s)){
+            if( *p== '*') return isMatch(s,p+1);
+            else if(*(p+1) == '*') return isMatch(s,p+2);
+
+            else {printf("eeeeeee %c %c\n",*p,*(p+1));return false;}
+        }else if(isNULL(p)){
+                printf("eddd\n");
+                return false;
+        }else{
+            if(*p=='*'){
+                if(istrue(p-1,s)) return isMatch(s+1,p) || isMatch(s,p+1) || isMatch(s+1,p+1) ;  // 匹配到前者
+                else return (isMatch(s,p+1) || isMatch(s-1,p+1));
+            }else{
+                if(*(p+1)=='*') return isMatch(s,p+2);
+                else {printf("hell %c %c %c \n",*p,*(p+1),*s);return false;}
+            }
+        }
+    }
+}
+
 int main(){
     char p[]="a";
-    char q[]="ab*";
+    char q[]="a*a";
     printf("%d\n",isMatch(p,q));
 
 }
