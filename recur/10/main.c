cbfe8b95c6e45e76bbfe44665b39f526d3b19bd6
s3cana <18374819636@163.com>
2022-03-18 18:03:49 +0800

    完成 正则

--- recur/10/main.c
+++ recur/10/main.c
@@ -10,20 +10,22 @@ bool isNULL(char *p){
     return *p == '\0';
 }
 bool isMatch(char *s ,char* p){
-    if(*s == *p || *p== '.'){
+    if(*s == *p || (*p== '.' && *s!='\0')){
         if(*p=='\0' && *s== '\0'){
             return true;
         }else if (isNULL(s)){
             return isMatch(s,p+1);
         }else if(isNULL(p)){
             return isMatch(s+1,p);
-        }else return isMatch(s+1,p+1);
+        }else if(*(p+1)=='*'){
+            return isMatch(s+1,p+1) || isMatch(s,p+2);
+        }else
+            return isMatch(s+1,p+1);
     }else{
         if(isNULL(s)){
             if( *p== '*') return isMatch(s,p+1);
             else if(*(p+1) == '*') return isMatch(s,p+2);
-
-            else {printf("eeeeeee %c %c\n",*p,*(p+1));return false;}
+            else return false;
         }else if(isNULL(p)){
                 printf("eddd\n");
                 return false;
@@ -40,8 +42,9 @@ bool isMatch(char *s ,char* p){
 }
 
 int main(){
-    char p[]="a";
-    char q[]="a*a";
+    char p[]="bbbba";
+    //char q[]= ".*..a*";
+    char q[] =".*a*a";
     printf("%d\n",isMatch(p,q));
 
 }
