a18951c4f23583b5ba8d62d070db9faf0593366d
s3cana <18374819636@163.com>
2022-03-18 15:09:08 +0800

    添加 +1

--- recur/10/main.c
+++ recur/10/main.c
@@ -2,39 +2,51 @@
 #define bool int
 #define false 0
 #define true 1
+bool istrue(char *p,char *s){
+    return  *p== *s || *p == '.'; 
+
+}
+
 bool isMatch(char * s, char * p){
-
-    if(*(s+1) =='\0' && (*s==*p|| *p=='*' || *p=='.')){
+    if(*(p+1)=='\0' && *(s+1) == '\0' && (istrue(p,s) ||(istrue(p-1,s) && *p=='*'))){
         return true;
-    }
-
-    //printf("start compare : %c%c\n",*s,*p);
-    if(*p == *s || *p == '.'){
-        //printf("start1 compare 1: %c %c\n",*s,*p);
-        return isMatch(++s ,++p);
-    }else if(*p != *s && *p != '*' && *(p+1)!='*'){
-        return false;
-    }else if(*p != *s && *(p+1)=='*'){
-        p = p + 2;
-        return isMatch(s,p);
-    }
-    else if(*p == '*' && (*s == *(p-1) || *(p-1) == '.')){
-        return isMatch(++s,p);
-    }else if(*p =='*' && *s != *(p-1) && *(p-1)!='.'){
-        //return isMatch(++s,++p);
-        return false;
     }else{
-        return isMatch(++s,++p);
+        if( *p == *s || *p == '.'){
+            if(*(s+1)!='\0' && *(p+1)!='\0')
+                return isMatch(s+1,p+1);
+        }else if(*p != *s && *p == '*'){
+            if( istrue(p-1,s) && istrue(p+1,s+1)){//后面一个也匹配到了
+                if(*(s+1)!='\0' && *(p+1) !='\0')
+                return isMatch(s+1,p+1);
+            }else if(istrue(p-1,s) && !istrue(p+1,s+1)){
+                /*前一个匹配到了，后一个没匹配到 */
+                bool a=0,b=0;
+                if(*(p+1)!='\0'){
+                    a = isMatch(s,p+1);
+                }
+                if(*(s+1)!='\0'){
+                    b = isMatch(s+1,p);// '\0' *
+                }
+                return a || b;
+            }else if(!istrue(p-1,s)){
+                if(*(p+1)!='\0')
+                    return isMatch(s,p+1);
+            }
+        }else if(*p!=*s&&*(p+1)!='\0'&&*(p+1)=='*'){
+            p++;
+            printf("eee\n");
+            if(*(p+1)!='\0')
+            return isMatch(s,++p);
+        }else{
+            return false;
+        }
+
     }
-    /*
-    aababbbbbbbaaab
-    .ab*aaab
-    */
-    
+    return false;
 }
 int main(){
-    char p[]="aab";
-    char q[]="c*a*b";
+    char p[]="a";
+    char q[]="ab*";
     printf("%d\n",isMatch(p,q));
 
 }
